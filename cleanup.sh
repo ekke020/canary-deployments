77a524f38da290a81ba3af7011062ef5c8f852e2
Johan Ekman <ekman-johan020@hotmail.com>
2022-12-16 16:17:41 +0100

    Added a demo app

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-kubectl delete -f ./platform/flagger/kustomization.yaml
+kubectl delete ServiceAccount flagger -n linkerd
+kubectl delete ClusterRoleBinding flagger -n linkerd
+kubectl delete ClusterRole flagger -n linkerd
+kubectl delete CustomResourceDefinition canaries.flagger.app
+kubectl delete CustomResourceDefinition metrictemplates.flagger.app
+kubectl delete CustomResourceDefinition alertproviders.flagger.app
+kubectl delete deployment flagger -n linkerd
+wait
+echo Uninstalled flagger
 
 linkerd viz uninstall | kubectl delete -f -
 wait
