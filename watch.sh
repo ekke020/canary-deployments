41749aaf2347e9e20e6599d3e9207f5f1848c8b0
Johan Ekman <ekman-johan020@hotmail.com>
2022-12-18 21:39:24 +0100

    Added some different options

--- watch.sh
+++ watch.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-watch 'kubectl get pods --all-namespaces --field-selector metadata.namespace!=kube-system'
+if [[ "$1" == "1" ]]; then
+  watch flux get kustomizations
+elif [[ "$1" == "2" ]]; then
+  watch kubectl get pods --all-namespaces --field-selector metadata.namespace!=kube-system
+elif [[ "$1" == "3" ]]; then
+  watch kubectl -n backend get canary
+elif [[ "$1" == "4" ]]; then
+  kubectl port-forward svc/frontend-srv 7878:80 -n frontend
+fi
