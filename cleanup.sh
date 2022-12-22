20a066aef32ef794bf6ea6b9cc68214508a80968
Johan Ekman <ekman-johan020@hotmail.com>
2022-12-22 09:30:20 +0100

    Taking down deployment

--- cleanup.sh
+++ cleanup.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-kustomize build raw-configs/canary-frontend | kubectl delete -f -
-wait
-echo Uninstalled the frontend
+# kustomize build raw-configs/canary-frontend | kubectl delete -f -
+# wait
+# echo Uninstalled the frontend
 
-kustomize build raw-configs/canary-backend | kubectl delete -f -
-wait
-echo Uninstalled the backend
+# kustomize build raw-configs/canary-backend | kubectl delete -f -
+# wait
+# echo Uninstalled the backend
 
-kubectl delete ServiceAccount flagger -n linkerd
-kubectl delete ClusterRoleBinding flagger -n linkerd
-kubectl delete ClusterRole flagger -n linkerd
-kubectl delete CustomResourceDefinition canaries.flagger.app
-kubectl delete CustomResourceDefinition metrictemplates.flagger.app
-kubectl delete CustomResourceDefinition alertproviders.flagger.app
-kubectl delete deployment flagger -n linkerd
+kustomize build platform/flagger/ | kubectl delete -f -
 wait
 echo Uninstalled flagger
 
-linkerd smi uninstall | kubectl delete -f -
+kustomize build platform/linkerd/smi | kubectl delete -f -
 wait
 echo Uninstalled linkerd smi
 
